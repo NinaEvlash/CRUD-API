83a3889ebdc88c24b379fed41d03bfa49038d183
Нина Евлаш <evlash.nina@mail.ru>
2025-11-07 00:47:45 +0100

    feat: add server.ts code

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,19 @@
-console.log('Hello world!');
+import http from 'http';
+import dotenv from 'dotenv';
+import { handleUsers } from './controllers/usersController';
+
+dotenv.config();
+const PORT = process.env.PORT ? Number(process.env.PORT) : 4000;
+
+const server = http.createServer((req, res) => {
+  if (req.url?.startsWith('/api/users')) {
+    handleUsers(req, res);
+  } else {
+    res.writeHead(404);
+    res.end(JSON.stringify({ message: 'Endpoint not found!' }));
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
